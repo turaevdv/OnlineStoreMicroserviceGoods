a94ac66e86fed7086b860b657654b4aadd05755a
Turaev <denis.turaev@bk.ru>
2022-08-11 12:52:08 +0300

    Initial commit

---
+++ src/main/java/ru/turaev/goods/GoodsApplication.java
@@ -0,0 +1,13 @@
+package ru.turaev.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GoodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class, args);
+    }
+}
